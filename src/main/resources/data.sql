0cfb25a242c78901806348dc18ede71073ce7421
Lucas-270 <61168952+Lucas-270@users.noreply.github.com>
2021-11-01 18:59:38 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tb_task (
+CREATE TABLE task (
 	id bigint PRIMARY KEY auto_increment,
 	title varchar(200),
 	description TEXT,
@@ -7,7 +7,7 @@ CREATE TABLE tb_task (
 	user_id int
 );
 
-CREATE TABLE tb_user (
+CREATE TABLE user (
 	id bigint PRIMARY KEY auto_increment,
 	name varchar(200),
 	email varchar(200),
@@ -15,28 +15,28 @@ CREATE TABLE tb_user (
 	githubuser varchar(200)
 );
 
-CREATE TABLE tb_role (
+CREATE TABLE role (
 	id int primary key auto_increment,
 	name varchar(200)
 );
 
-CREATE TABLE tb_user_roles(
+CREATE TABLE user_roles(
 	user_id int,
 	roles_id int
 );
 
-INSERT INTO tb_role (name) VALUES ('ROLE_ADMIN'), ('ROLE_USER');
+INSERT INTO role (name) VALUES ('ROLE_ADMIN'), ('ROLE_USER');
 
-INSERT INTO tb_user_roles VALUES (1, 1);
+INSERT INTO user_roles VALUES (1, 1);
 
 
-INSERT INTO tb_user (name, email, password, githubuser) VALUES
+INSERT INTO user (name, email, password, githubuser) VALUES
 ('Joao Carlos', 'joao@gmail.com', '$2a$12$O2BXknIGzw4aqG52PMfVkut/8/pFrfAo1ZpHsNdZy6Tvb3I4vZ2Oe', 'joaocarloslima'),
 ('Carla Lopes', 'carla@gmail.com', '$2a$12$O2BXknIGzw4aqG52PMfVkut/8/pFrfAo1ZpHsNdZy6Tvb3I4vZ2Oe', 'carla'),
 ('Fabio Cabrini', 'fabio@fiap.com.br', '$2a$12$O2BXknIGzw4aqG52PMfVkut/8/pFrfAo1ZpHsNdZy6Tvb3I4vZ2Oe', 'jose');
 
 
-INSERT INTO tb_task (title, description, points, status, user_id) VALUES 
+INSERT INTO task (title, description, points, status, user_id) VALUES 
 	('Criar banco de dados',
 	'Criar um banco de dados na nuvem com Oracle', 
 	200,
@@ -44,8 +44,8 @@ INSERT INTO tb_task (title, description, points, status, user_id) VALUES
 	1);
 	
 	
-INSERT INTO tb_task (title, description, points, status) VALUES 
+INSERT INTO task (title, description, points, status) VALUES 
 	('Protótipo','Criação de protótipo de alta fidelidade', 100, 50);
 	
-INSERT INTO tb_task (title, description, points, status, user_id) VALUES 
+INSERT INTO task (title, description, points, status, user_id) VALUES 
 	('API REST','Publicação de API com endpoints da aplicação', 150, 95, 2);
